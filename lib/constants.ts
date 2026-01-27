9b16cfa158560648871a980af73e9e6f7b907414
Hasan Çelik <hsn.2903@outlook.com>
2026-01-27 13:18:28 +0300

    constants.ts updated

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,9 @@
-export const APP_NAME = "Fuarlarım";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Fuarlarım";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Dünyadaki fuarlardan haberdar olun";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 import { IconType } from "react-icons";
 import { MdCabin } from "react-icons/md";
